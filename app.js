dcd40b34b1979e46def27ddb34d6127fddc6e760
atemp21 <aaron.templeton@live.com>
2019-07-15 16:45:39 -0600

    added async

--- app.js
+++ app.js
@@ -8,57 +8,66 @@ const port = process.env.PORT || 8080
 const AuthToken = process.env.AUTH_TOKEN;
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
 // parse application/json
 app.use(bodyParser.json())
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('Message Terminator for Slack')
 });
 
-app.post('/', function(req, res){
+app.post('/', async function (req, res) {
     var payload = JSON.parse(req.body.payload);
     var channel = payload.channel.id;
     var user = payload.user.id;
-    //console.log(channel+" "+user)
-    getUsersMessagesInChannel(channel, user);
-    res.sendStatus(200);
+    var response_url = payload.response_url;
+
+ await axios.post(response_url, {
+        text: "Deleting your messages",
+        response_type: 'ephermal'
+    })
+    await getUsersMessagesInChannel(channel, user, response_url);
+    res.sendStatus(200)
+   
+
 });
 
-function getUsersMessagesInChannel(channel, user){
-    
-    axios.get('https://slack.com/api/conversations.history?token='+AuthToken+'&channel='+channel).then((res)=>{
+async function getUsersMessagesInChannel(channel, user, r) {
+
+    await axios.get('https://slack.com/api/conversations.history?token=' + AuthToken + '&channel=' + channel).then((res) => {
         var timestamps = [];
         var messages = res.data.messages;
-        //console.log(messages)
+        console.log(messages)
         messages.forEach(m => {
-            if(m.user === user){
+            if (m.user === user) {
                 timestamps.push(m.ts);
             }
         });
-        //console.log(timestamps)   
-        deleteUserMessages(channel, timestamps);    
+
+        deleteUserMessages(channel, timestamps, r);
     })
 
 }
 
-function deleteUserMessages(channel, timestamps){
+async function deleteUserMessages(channel, timestamps, url) {
 
-   timestamps.forEach(t => {
-        axios.post('https://slack.com/api/chat.delete',
-        {
+    await timestamps.forEach(t => {
+        axios.post('https://slack.com/api/chat.delete', {
             channel: channel,
             ts: t
-        },
-        {
-          headers:{  Authorization: "Bearer "+AuthToken}
-        }
-    ).then((res)=>{
-        //console.log(res)
+        }, {
+            headers: {
+                Authorization: "Bearer " + AuthToken
+            }
+        }).then((res) => {
+
+        });
     });
-   });
+
 }
 
-app.listen(port,()=>console.log('server running on port: '+port));
+app.listen(port, () => console.log('server running on port: ' + port));
